2f38b353e70120a9fc683258928e9721661e582b
gitvijayy <7822vijay@gmail.com>
2019-07-23 11:34:27 -0700

    python

--- javascript.js
+++ javascript.js
@@ -19,7 +19,7 @@ console.log(hourglass(arr))
 
 let arr = [1, 2, 3, 4, 5]
 
-function rotLeft(a, d) {
+const rotLeft = (a, d) => {
 
   for (var i = 0; i < d; i++) {
     a.push(a[0])
@@ -31,3 +31,32 @@ function rotLeft(a, d) {
 }
 
 console.log(rotLeft(arr, 4))
+
+// New Year Chaos
+
+const minimumBribes = (q) => {
+
+  let bribes = 0
+  for (var i = 0; i < q.length; i++) {
+    if (q[i] - i > 3) {
+      console.log("Too chaotic")
+      return "Too chaotic"
+    }
+    if (q[i] - i > 2) {
+      bribes = bribes + 2
+    } else {
+      for (var j = i; j < q.length; j++) {
+        if (q[i] > q[j]) {
+          bribes++
+        }
+
+      }
+    }
+  }
+  console.log(bribes)
+  return bribes
+
+}
+
+console.log(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4]))
+console.log(minimumBribes([1, 2, 5, 3, 4, 7, 8, 6])) 
